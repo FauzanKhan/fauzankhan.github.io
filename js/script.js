055570059a6a4ba4d864ad7e71f855c365bd8f3b
FauzanKhan <fauzankhan93@gmail.com>
2014-10-15 18:02:14 +0530

    Fixed Broken Links

--- js/script.js
+++ js/script.js
@@ -41,8 +41,7 @@ $(document).ready(function(){
 			$('.back_to_top').fadeOut();
 			}
 	});
-	
-	
+
 	$(function() {
 	  $('a[href*=#]:not([href=#])').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
